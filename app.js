1f614b917060ec2fff2d54755645e64d06f86794
Peter <pedrobadia@gmail.com>
2021-05-27 10:18:01 -0300

    First Commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const { homedir } = require('os');
+const path = require('path');
+
+const app = express();
+
+const publicPath = path.resolve(__dirname, './public');
+app.use (express.static (publicPath) );
+
+app.listen(3000, () => {
+    console.log('Servidor on line puerto 3000')
+});
+
+app.get('/', (req, res)=> {
+    res.sendFile (path.resolve(__dirname, './views/home.html'));
+})
