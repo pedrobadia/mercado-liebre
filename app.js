d468d06f262e1722a641e27ad2200a315fd18698
Peter <pedrobadia@gmail.com>
2021-06-10 12:31:39 -0300

    Se agrego post a la home  en app.js

--- app.js
+++ app.js
@@ -15,6 +15,10 @@ app.get('/', (req, res)=> {
     res.sendFile (path.resolve(__dirname, './views/home.html'));
 })
 
+app.post('/', (req, res)=> {
+    res.sendFile (path.resolve(__dirname, './views/home.html'));
+})
+
 app.get('/register.html', (req, res)=> {
     res.sendFile (path.resolve(__dirname, './views/register.html'));
 })
